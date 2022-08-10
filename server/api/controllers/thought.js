89e2197611ac9fe4b152099723a00bf8866ddc94
madCakes <jason.pbrooks@hotmail.com>
2022-08-10 15:39:47 +0100

    update create, add exports

--- server/api/controllers/thought.js
+++ server/api/controllers/thought.js
@@ -2,16 +2,28 @@ const Thought = require("../models/Thought");
 
 async function create(req, res) {
   try {
-    const title = req.body.title;
-    const author = req.body.author;
-    const post = req.body.post;
+    const { title } = req.body;
+    const { author } = req.body;
+    const { post } = req.body;
 
     const thought = await Thought.create({ title, author, post });
-
-    res.status(201).json(thought);
+    console.log();
+    res.status(201).json({
+      thoughts: thought,
+    });
   } catch (err) {
     res.status(422).json({
       error: err,
     });
   }
 }
+
+// async function show(req, res) {
+//   try {
+//     const thought = await thought.findByURL(req.params)
+//   }
+// }
+
+module.exports = {
+  create,
+};
