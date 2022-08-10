051b61134a16ad970f60d1631c7b55ba9d814a72
CallumCheng <callumcheng@outlook.com>
2022-08-10 17:30:47 +0100

    attempted POST/GET from server

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,29 +1,44 @@
 const form = document.getElementById('form');
 
-const displayTitle = document.getElementsByClassName('display-title');
-const displayName = document.getElementsByClassName('display-name');
-const displayContent = document.getElementsByClassName('display-content');
+//May need to change root URL
+const rootUrl = 'http://localhost:3000/results';
 
-form.addEventListener('submit', newPost)
+form.addEventListener('submit', newPost, sendPostToServer);
 
 function newPost(e) {
-    e.preventDefault();
-    //Grab post from API URL
-    const url = '';
-    
-    fetch(url)
-        .then 
 
-    let title = document.getElementById('post-title')
+    e.preventDefault();
+    const title = document.getElementById('post-title')
         console.log(title.value)
-    let name = document.getElementById('post-name')
+    const name = document.getElementById('post-name')
         console.log(name.value)
-    let content = document.getElementById('post-content')
+    const content = document.getElementById('post-content')
         console.log(content.value)
 
-        displayTitle.innerHTML = title.value
-        displayName.innerHTML = name.value
-        displayContent.innerHTML = content.value
+
+}
+
+function sendPostToServer(form) {
+    fetch(rootUrl,
+        {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(form)
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => console.log(err))
+}
 
 
-    }
+//Grab post from API URL
+fetch(rootUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.log(err));
