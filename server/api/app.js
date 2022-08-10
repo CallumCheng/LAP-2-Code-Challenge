c56b96ec0ff61f8e701ca29db25640400ddc2c8e
madCakes <jason.pbrooks@hotmail.com>
2022-08-10 12:05:19 +0100

    update layout, add home route and middleware

--- server/api/app.js
+++ server/api/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
 const thoughtRoutes = require("./routes/thought");
 
-app.use(cors());
-
 app.use("/", thoughtRoutes);
+app.get("/", (req, res) => res.send("Welcome to Thoughts.FM"));
 
 module.exports = app;
