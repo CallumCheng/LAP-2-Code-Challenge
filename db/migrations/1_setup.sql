2973cb063bccfec50a2fba18a919dd7f5ca51a95
madCakes <jason.pbrooks@hotmail.com>
2022-08-10 17:31:16 +0100

    update file paths

--- db/migrations/1_setup.sql
+++ db/migrations/1_setup.sql
@@ -4,5 +4,5 @@ CREATE TABLE thought (
     id SERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     author VARCHAR(50) NOT NULL,
-    post VARCHAR(500) NOT NULL,
+    post VARCHAR(500) NOT NULL
 );
