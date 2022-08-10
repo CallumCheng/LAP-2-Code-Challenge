7c19299beac259f9e77cd4a5c1c87266c7cded04
madCakes <jason.pbrooks@hotmail.com>
2022-08-10 18:03:56 +0100

    add get route

--- api/routes/thought.js
+++ api/routes/thought.js
@@ -4,6 +4,7 @@ const thoughtController = require("../controllers/thought");
 const thoughtRouter = Router();
 
 // thoughtRouter.get("/", thoughtController.show);
+thoughtRouter.get("/:id", thoughtController.show);
 thoughtRouter.post("/", thoughtController.create);
 
 module.exports = thoughtRouter;
