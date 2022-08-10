70e352c00e83299b5da13453424087444f4832f9
madCakes <jason.pbrooks@hotmail.com>
2022-08-10 16:11:03 +0100

    add SQL setup

---
+++ server/db/migrations/1_setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS thought;
+
+CREATE TABLE thought (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    post VARCHAR(500) NOT NULL,
+);
