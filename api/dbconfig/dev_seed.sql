e75b7258b3f5af726d209e4179c5bb4ccce4b87f
madCakes <jason.pbrooks@hotmail.com>
2022-08-11 05:23:05 +0100

    update dbconfig, add seed data

---
+++ api/dbconfig/dev_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO thought (title, author, post)
+VALUES
+('Test title_1', 'Test author_1', 'Test post_1'),
+('Test title_2', 'Test author_2', 'Test post_2');
--- api/models/Thought.js
+++ api/models/Thought.js
@@ -1,4 +1,4 @@
-const db = require("../dbConfig");
+const db = require("../dbconfig/init");
 const path = require("path");
 
 class Thought {
